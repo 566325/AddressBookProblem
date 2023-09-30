7f871d1b4f07d60e5388cfc15910dca75a5afdc6
566325 <nethisireesha288@gmail.com>
2023-09-30 17:41:09 +0530

    [Sireesha]:Added Create cantacts in Address Book

---
+++ Contacts.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class person
+    {
+        public string FirstName {get;set;}
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZIPCode { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email{ get; set; }
+
+
+
+        private bool ValidateFullName(string firstName, string lastName)
+        {
+            try
+            {
+                if (firstName != " " && lastName != " ")
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Full Name");
+            }
+            return false;
+        }
+
+        private bool ValidateAddress(string address, string city, string state, int zipCode)
+        {
+            try
+            {
+                if (address != " " && city != " " && state != " " && zipCode != 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Address");
+            }
+            return false;
+        }
+        private bool ValidateContactNumbers(long phoneNumber, string email)
+        {
+            try
+            {
+                if (phoneNumber != 0 && email != " ")
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Contact Numbers");
+            }
+            return false;
+        }
+        public void ValidateContactDetails()
+        {
+            try
+            {
+                bool full_Name = ValidateFullName(FirstName, LastName);
+                bool addr = ValidateAddress(Address, City, State, ZIPCode);
+                bool contact = ValidateContactNumbers(PhoneNumber, Email);
+                if (full_Name == true && addr == true && contact == true)
+                    Console.WriteLine("Contact Details Saved Successfully");
+                else
+                    Console.WriteLine("Contact Save Failed!!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Saving Details");
+            }
+
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wlcome to The Address Book");
+            person contacts = new person
+            {
+                FirstName = "Sireesha",
+                LastName = "Nethi",
+                Address = "Ameerpet",
+                City = "Hyd",
+                State = "Telangana",
+                ZIPCode = 500016,
+                PhoneNumber = 1234567890,
+                Email = "Sireesha@gmail.com"
+            };
+            contacts.ValidateContactDetails();
+            Console.WriteLine("Contact Details: ");
+            Console.WriteLine($"Full Name: {contacts.FirstName + contacts.LastName} ");
+            Console.WriteLine($"Phone Number: {contacts.PhoneNumber} ");
+            Console.WriteLine($"Address: {contacts.Address} \n \t{contacts.City} {contacts.State} \n \t{contacts.ZIPCode} ");
+            Console.WriteLine($"Email: {contacts.Email} ");
+
+        }
+    }
+}
